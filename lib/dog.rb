f01c5b718446dc9bda52bb09c6da84d898ff41ac
Marilexia Faith Acelajado <faithacelajado@gmail.com>
2018-06-13 00:58:41 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,14 +9,16 @@ def initialize(name)
   @@all << self
 end
 
-def self.clear_all
-  @@all.clear
-end
 
 def self.all
   @@all.each do |dog|
     puts dog.name
   end
+end
+
+
+def self.clear_all
+  @@all.clear
 end
 
 end
